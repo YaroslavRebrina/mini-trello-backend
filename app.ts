2a4a3f52fc83cca97f0ab2b6d446f66d3df126db
Yaroslav Rebrina <yaroslavrebrina@gmail.com>
2025-05-07 13:58:56 +0300

    fix

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './src/routes';
 import { errorHandler } from './middlewares/error.middleware';
@@ -7,15 +7,41 @@ const app: Application = express();
 
 app.use(
  cors({
-  origin: process.env.CORS_ORIGIN || '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: [
+   'Content-Type',
+   'Authorization',
+   'X-Requested-With',
+   'Accept',
+   'Origin',
+  ],
+  exposedHeaders: ['Content-Range', 'X-Content-Range'],
   credentials: true,
+  maxAge: 86400,
  })
 );
 
+app.options('*', (req: Request, res: Response) => {
+ res.header('Access-Control-Allow-Origin', '*');
+ res.header(
+  'Access-Control-Allow-Methods',
+  'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
+ );
+ res.header(
+  'Access-Control-Allow-Headers',
+  'Content-Type, Authorization, X-Requested-With, Accept, Origin'
+ );
+ res.status(200).end();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get('/health', (req: Request, res: Response) => {
+ res.status(200).json({ status: 'ok', message: 'Server is running' });
+});
+
 app.use('/api', routes);
 app.use(errorHandler);
 
